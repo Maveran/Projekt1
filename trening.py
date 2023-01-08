477f60298ccf516ab24b082f6d04e0412a4b8433
Kuba <jakub.chuchmacz@gmail.com>
2023-01-08 18:41:50 +0100

    inicjalizacja repo

---
+++ trening.py
@@ -0,0 +1,22 @@
+# list2 = ["apple", "orange"]
+# tuple2 = ("kiwi", "orange")
+# list2.extend(tuple2)
+# print(list2)
+
+# for i in list2:
+#     print(i)
+
+inputlist = []
+
+i=0
+while i < 4:
+
+    a = int(input(str("please input your number ")))
+    print("your number is: " + str(a))
+
+    
+
+    inputlist.append(a)
+
+    print("inputs", inputlist)
+    i += 1;
